c93331f0f588312a890a4d73cfc965f969dfeff0
Maksss2018 <malyimaksym37@gmail.com>
2019-04-25 15:46:06 +0300

    initial commit

---
+++ src/ac/coursesActions.js
@@ -0,0 +1,41 @@
+import C from "../constants"
+import * as courseApi from "../api/courseApi"
+
+export const loadCoursesAction = () => {
+  return function(dispatch) {
+    return courseApi
+      .getCourses()
+      .then(courses => {
+        dispatch({
+          type: C.LOAD_COURSES_SUCCESS,
+          payload: courses,
+        })
+      })
+      .catch(err => {
+        throw err
+      })
+  }
+}
+
+export const createSuccessCourse = course => ({
+  type: C.CREATE_COURSE_SUCCESS,
+  payload: course,
+})
+
+export const updateSuccessCourse = course => ({
+  type: C.UPDATE_COURSE_SUCCESS,
+  payload: course,
+})
+
+export const saveCoursesAction = course => dispatch => {
+  return courseApi
+    .saveCourse(course)
+    .then(savedCourse => {
+      course.id
+        ? dispatch(updateSuccessCourse(savedCourse))
+        : dispatch(createSuccessCourse(savedCourse))
+    })
+    .catch(err => {
+      throw err
+    })
+}
