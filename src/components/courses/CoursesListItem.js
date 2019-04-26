6e53d682276f523a2a2db6b7568be8d86d9ca377
Maksss2018 <malyimaksym37@gmail.com>
2019-04-26 11:53:32 +0300

    - isValid  function  = complete not finished : - errors msg + toads = complete/not finished - delete item =  complete

--- src/components/courses/CoursesListItem.js
+++ src/components/courses/CoursesListItem.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react"
+import React from "react"
 import {connect} from "react-redux"
 import {deleteCourse} from "../../ac/coursesActions"
 import {Link} from "react-router-dom"
