8ae047eb179f361707f0634d0b372e62c543bcc1
Makss2018 <malyimaksum37@gmail.com>
2019-11-05 15:32:20 +0200

    no store needed

--- js/store.js
+++
@@ -1,9 +0,0 @@
-const store = function(state, fn){
-    this.state ={ ...state };
-    this.getState = () => ({...this.state});
-    this.setState = (state) => {
-        this.state = {...this.state, ...state}
-    };
-    return fn({ ...this})
-}
-/*TODO implement common state to the other parts  of  native code*/
